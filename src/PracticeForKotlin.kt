cfe5493647991355bc3cb11e166db86e572348dc
Tijil2111 <tijil2008@gmail.com>
2021-06-23 14:14:42 +0530

    Initial commit

---
+++ src/PracticeForKotlin.kt
@@ -0,0 +1,12 @@
+fun main(){
+    var a = 5
+    val b= 3.0
+
+    println("a + b is ${a + b}")
+    println("a - b is ${a - b}")
+    println("a * b is ${a * b}")
+    println("a / b is ${a / b}")
+    println("a % b is ${a % b}")
+
+    println(" (2 + 2) x 3 = ${(2 + 2) * 3}")
+}
